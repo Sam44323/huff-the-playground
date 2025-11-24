d7ef49af74823680355de8cdf16424f1fc198bdc
Suranjan <suranjanbiswas2002@gmail.com>
2025-11-24 22:20:13 +0530

    add: added initial-conditional jumps

--- src/horseStoreV1/HorseStore.sol
+++ src/horseStoreV1/HorseStore.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.20;
 contract HorseStore {
   uint256 numberOfHorses;
 
+  // 0xcdfead2e
   function updateHorseNumber(uint256 newNumberOfHorses) external {
     numberOfHorses = newNumberOfHorses;
   }
 
+  // 0xe026c017
   function readNumberOfHorses() external view returns (uint256) {
     return numberOfHorses;
   }

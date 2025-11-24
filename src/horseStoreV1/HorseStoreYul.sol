1e12c052bc7f3b25b721b2503cdb25b1c1e7dca9
Suranjan <suranjanbiswas2002@gmail.com>
2025-11-25 02:34:32 +0530

    add: added the file for yul

---
+++ src/horseStoreV1/HorseStoreYul.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract HorseStoreYul {
+  uint256 numberOfHorses;
+
+  // 0xcdfead2e
+  function updateHorseNumber(uint256 newNumberOfHorses) external {
+    numberOfHorses = newNumberOfHorses;
+  }
+
+  // 0xe026c017
+  function readNumberOfHorses() external view returns (uint256) {
+    return numberOfHorses;
+  }
+}
